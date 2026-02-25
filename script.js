f18af6f9be2c0d8cd222f1a4e2c556315ec7a4a9
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2026-02-25 10:40:36 -0300

    type coercion

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-let country = 'Brazil';
-let continent = 'South America';
-let population = 131412132;
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear));
+console.log(Number(inputYear) + 18);
 
-console.log(country);
-console.log(continent);
-console.log(population);
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion (all the same below)
+console.log('I am ' + 23 + ' years old');
+console.log('I am ' + '23' + ' years old');
+console.log('I am ' + String(23) + ' years old');
+
+console.log('23' - '10' - 3); // the strings are converted automatically to number
+console.log('23' + '10' + 3);
